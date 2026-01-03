36b997b38e85e35ade268a6b4596275422346124
DanielKuhnDeAraujo <danielaprendendo00@gmail.com>
2026-01-03 19:25:00 -0300

    tudo lindo

--- scripts/tiro.gd
+++ scripts/tiro.gd
@@ -1,20 +1,54 @@
 extends CharacterBody2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-
+@onready var player = $"../Player"
 var dano =1 
-const SPEED = -100
-const gravidade = 90
+var SPEED = 0
+var gravidade = 405
+var subir : float  =232
+var tempo
+var pode_mover = true
+#pra ter vida
+var vida = 1
+@onready var vida_inimigo: Node = $VidaInimigo
+var spawner
 
+func _ready() -> void:
+	var difx : float=-( global_position.x - player.global_position.x)
+	if abs(difx) < 100 : 
+		subir=190
+	velocity.y=-subir
+	print(velocity.y)
+	#tempo de subida 
+	var t1: float = subir / gravidade
+	#altura maxima
+	var altura: float = subir * subir / (gravidade*2)
+	var dify: float  = global_position.y - player.global_position.y
+	dify=-dify
+	altura += dify
+	# tempo de queda
+	var antesraiz =(2*altura)/gravidade
+	var invert = false
+	if antesraiz < 0:
+		antesraiz = -antesraiz
+		invert = true
+	var t2 = sqrt(antesraiz)
+	tempo = t1 + t2
+	if invert :
+		tempo -=t2
+	SPEED =difx/tempo
+	
 func _physics_process(delta: float) -> void:
-
-	if !is_on_floor():
+	if pode_mover :
 		velocity.y+= gravidade *delta
-	velocity.x=SPEED
-	look_at(velocity+position)
-	var collision  = move_and_collide(velocity*delta)
-	if !collision==null :
-		queue_free()
+		velocity.x=SPEED
+		look_at(velocity+position)
+		var collision  = move_and_collide(velocity*delta)
+		if collision!=null :
+			queue_free()
 
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
+func _on_area_2d_body_entered(_body: Node2D) -> void:
 	queue_free() # Replace with function body.
+
+func interromper(estado: bool) -> void:
+	pode_mover = estado
