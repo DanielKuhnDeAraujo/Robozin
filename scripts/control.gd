736c2b22ee65a6afd585109c6997625971cf33bd
Flavio-117 <fvolacerda@hotmail.com>
2026-01-06 19:08:58 -0300

    its ova

--- scripts/control.gd
+++ scripts/control.gd
@@ -6,9 +6,17 @@ var last_hp: int = 5
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	get_tree().paused = false
+	visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _input(event: InputEvent) -> void:
+	if Input.is_action_pressed("pausa"):
+		if get_tree().paused:
+			get_tree().paused = false
+			visible = false
+		else:
+			visible = true
+			get_tree().paused = true
+		
